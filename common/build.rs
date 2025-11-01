24af42a24b5b7c856a7afb8e677b493dccd4e3da
Ananya Timalsina <84459091+ananyatimalsina@users.noreply.github.com>
2025-11-01 19:30:40 +0100

    Fix ui path misconfiguration in common/build.rs

--- common/build.rs
+++ common/build.rs
@@ -3,6 +3,7 @@ fn main() {
 
     let config = slint_build::CompilerConfiguration::new()
         .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer);
-    slint_build::compile_with_config("ui/main.slint", config).unwrap();
+    slint_build::compile_with_config("../ui/main.slint", config).unwrap();
     slint_build::print_rustc_flags().unwrap();
 }
+
